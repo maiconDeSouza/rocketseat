0a471b74008726fde692560bb598c846b350bbdf
Maicon Souza <maicon_souza09@hotmail.com>
2022-03-11 15:34:43 -0300

    estruturas de repeticoes - finalizado

---
+++ Discover/10-guia-estrelar-javascript/10-estrutura-de-repeticao/forIn.js
@@ -0,0 +1,9 @@
+const person = {
+    nome: 'Maicon',
+    idade: 33,
+    profissao: 'Programador'
+}
+
+for(let key in person){
+    console.log(person[key])
+}
---
+++ Discover/10-guia-estrelar-javascript/10-estrutura-de-repeticao/forOf.js
@@ -0,0 +1,11 @@
+const nomes = ['Dante', 'Felipe', 'Dona Maia', 'Pretinho', 'Belinha', 'Karu']
+
+for(let nome of nomes){
+    console.log(nome)
+}
+
+const nome = 'dante'
+
+for(let letra of nome){
+    console.log(letra)
+}
