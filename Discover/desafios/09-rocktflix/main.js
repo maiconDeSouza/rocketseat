b4063589c4ba1bf29506cf90acdf94cc5c15cd19
Maicon Souza <maicon_souza09@hotmail.com>
2022-04-08 20:21:41 -0300

    09-desafio rocketflix - 80 porcento finalizado

--- Discover/desafios/09-rocktflix/main.js
+++ Discover/desafios/09-rocktflix/main.js
@@ -7,41 +7,133 @@ import {
 const buttonPlay = document.querySelector('button')
 const capaFilme = document.querySelector('.capa-filme')
 const textoFilme = document.querySelector('.texto-filme')
-const img = document.querySelector('.capa-filme img')
-const p = document.querySelector('.texto-filme p')
+
 
 
 
 buttonPlay.addEventListener('click', e =>{
   e.preventDefault()
-  main()
+  // main()
+  getMovie()
 })
 
 
-
-const numero = 2
-
-async function main(){
-  if(capaFilme.childNodes[0]){
-    capaFilme.innerHTML =''
-  }
-  if(textoFilme.childNodes[0]){
-    textoFilme.innerHTML =''
-  }
- 
-  const capaImg = document.createElement('img')
-  const textoP = document.createElement('p')
+async function getMovie(){
+  const numero = Math.floor(Math.random() * (1000 - 1 + 1)) + 1
   const db = await fetch(`${BASE_URL}/${numero}?api_key=${API_KEY}&${language}`)
+  const status = await db.status
+  
   const dbJson = await db.json()
-
-  capaImg.setAttribute('src', `${IMG_URL}${dbJson.poster_path}`)
-  textoP.innerText = `${dbJson.overview}`
-  capaFilme.appendChild(capaImg)
-  textoFilme.appendChild(textoP)
-  
   
 
-  // document.querySelector('body').innerHTML = `<img src="${IMG_URL}/${dbJson.poster_path}" alt="">`
+  const img = await `${IMG_URL}${dbJson.poster_path}`
+  const text = await dbJson.overview
+
+  await createHTML(img, text, status)
 }
+
+function createHTML(img, text, status){
+  if(status === 404){return vaEstudar()}
+  if(!capaFilme.querySelector('img') && !textoFilme.querySelector('p')){
+    const tagImg = document.createElement('img')
+    const tagP = document.createElement('p')
+
+    tagImg.setAttribute('src', img)
+    tagP.innerText = text
+
+    capaFilme.appendChild(tagImg)
+    textoFilme.appendChild(tagP)
+  } else{
+    const tagImg = capaFilme.querySelector('img')
+    const tagP = textoFilme.querySelector('p')
+    
+    tagImg.setAttribute('src', img)
+    tagP.innerText = text
+
+  }
+
+  function vaEstudar(){
+    console.log('Vamos estudar')
+    const img = './assets/codar.jpg'
+    const text = 'Hoje sem Filmes, vamos codar!!!'
+    if(!capaFilme.querySelector('img') && !textoFilme.querySelector('p')){
+      const tagImg = document.createElement('img')
+      const tagP = document.createElement('p')
+  
+      tagImg.setAttribute('src', img)
+      tagP.innerText = text
+  
+      capaFilme.appendChild(tagImg)
+      textoFilme.appendChild(tagP)
+    } else{
+      const tagImg = capaFilme.querySelector('img')
+      const tagP = textoFilme.querySelector('p')
+  
+      tagImg.setAttribute('src', img)
+      tagP.innerText = text
+  
+    }
+  }
+
+
+  
+  
+
+  
+  
+
+  
+  
+}
+
+// async function main(){
+//   const numero = Math.floor(Math.random() * (1000 - 1 + 1)) + 1
+//   const db = await fetch(`${BASE_URL}/${numero}?api_key=${API_KEY}&${language}`)
+//   const dbJson = await db.json()
+  
+//   console.log(numero)
+//   const img = document.querySelector('.capa-filme  img')
+//   const p = document.querySelector('.texto-filme p')
+//   let capaImg;
+//   let textoP;
+ 
+//   if(!img){
+//     capaImg = document.createElement('img')
+//   }
+//   if(!p){
+//     textoP = document.createElement('p')
+//   }
+
+  
+
+//     if(img){
+//       capaImg.removeAttribute('src')
+//     }
+//     if(p){
+//       textoP.innerText = ''
+//     }
+ 
+//     console.log(dbJson.poster_path)
+//     capaImg.setAttribute('src', `${IMG_URL}${dbJson.poster_path}`)
+//     capaFilme.appendChild(capaImg)
+  
+    
+  
+//     textoP.innerText = `${dbJson.overview}`
+//     textoFilme.appendChild(textoP)
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+//   console.log('fim')
+
+//   // document.querySelector('body').innerHTML = `<img src="${IMG_URL}/${dbJson.poster_path}" alt="">`
+// }
 
 
--- Discover/desafios/09-rocktflix/readme.md
+++ Discover/desafios/09-rocktflix/readme.md
@@ -3,9 +3,9 @@
 Grande parte do projeto já adiantei. 
 
 Mas preciso melhorar:
-1- melhorar o código js
-2- resolver problema do botão  (esse problema acontece pq eu apago os itens que foram criados para criar outros com as novas informções) - mas já tenho a solução e é simples
+1- melhorar o código js - Concluído 
+2- resolver problema do botão  (esse problema acontece pq eu apago os itens que foram criados para criar outros com as novas informções) - mas já tenho a solução e é simples - Concluído
 
 E preciso entregar:
-1- Gerar um filme aletório, após cada click.
+1- Gerar um filme aletório, após cada click. - Concluido 
 2- responsividade
