001c1aa1ecfd74036318b2b527aa1e3395fd2d0f
Maicon Souza <maicon_souza09@hotmail.com>
2022-04-22 11:14:24 -0300

    desafio todos finalizado com testes

--- Ignite/node/chapter-I/03-to-do/template/ignite-template-conceitos-do-nodejs-main/src/index.js
+++ Ignite/node/chapter-I/03-to-do/template/ignite-template-conceitos-do-nodejs-main/src/index.js
@@ -10,25 +10,31 @@ app.use(express.json());
 
 const users = [];
 
-function checksExistsUserAccount(req, res, next) {
-  const {username} = req.headers
+function checksExistsUserAccount(request, response, next) {
+  const {username} = request.headers
 
-  const user = users.find(user => user.username === username)
-  if(!user){return res.status(404).json({error: 'Usuário ainda não cadastrado'})}
+  const user  = users.find(user => user.username === username)
 
-  req.user = user
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  request.user = user
 
   return next()
 }
 
-app.post('/users', (req, res) => {
-  const {name, username} = req.body
+app.post('/users', (request, response) => {
+  const {name, username} = request.body
+  
+  const userExists = users.find(user => user.username === username)
 
-  const userExists = user.some(user => user.username === username)
-  if(userExists){return res.status(400).json({error: 'usuario já existe'})}
+  if(userExists){
+    return response.status(400).json({error:"Username already exists"})
+  }
 
   const user = { 
-    id: uuidv4(),
+    id: uuidv4(), 
     name, 
     username, 
     todos: []
@@ -36,21 +42,21 @@ app.post('/users', (req, res) => {
 
   users.push(user)
 
-  res.status(201).json(user)
+  return response.status(201).json(user)
 });
 
-app.get('/todos', checksExistsUserAccount, (req, res) => {
-  const { user} = req
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const {user} = request
 
-  return res.json(user.todos)
+  return response.json(user.todos)
 });
 
-app.post('/todos', checksExistsUserAccount, (req, res) => {
-  const {user}  = req
-  const {title, deadline} = req.body
-
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const {user} = request
+  const {title, deadline} = request.body
+  
   const todo = { 
-    id: uuidv4(), 
+    id: uuidv4(),
     title,
     done: false, 
     deadline: new Date(deadline), 
@@ -58,48 +64,56 @@ app.post('/todos', checksExistsUserAccount, (req, res) => {
   }
 
   user.todos.push(todo)
-  return res.status(201).json(user)
+
+  return response.status(201).json(todo)
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
-  const {user}  = req
-  const {title, deadline} = req.body
-  const {id} = req.params
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const {user} = request
+  const {title, deadline} = request.body
+  const {id} = request.params
 
   const todo = user.todos.find(todo => todo.id === id)
 
-  if(!todo){return res.status(404).json({error:"Tarefa não encontarada"})}
+  if(!todo){
+    return response.status(404).json({error: "Todo not found"})
+  }
 
   todo.title = title
   todo.deadline = new Date(deadline)
 
-  res.status(200).json(todo)
-
+  return response.status(200).json(todo)
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
-  const {user}  = req
-  const {id} = req.params
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const {user} = request
+  const {id} = request.params
 
   const todo = user.todos.find(todo => todo.id === id)
-  if(!todo){return res.status(404).json({error:"Tarefa não encontarada"})}
 
-  todo.done = true
-  return res.status(200).json(todo)
+  if(!todo){
+    return response.status(404).json({error: "Todo not found"})
+  }
+
+  todo.done = !todo.done
+
+  return response.status(200).json(todo)
 
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const {user}  = req
-  const {id} = req.params
+  const {user} = request
+  const {id} = request.params
 
   const todo = user.todos.find(todo => todo.id === id)
-  if(!todo){return res.status(404).json({error:"Tarefa não encontarada"})}
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found"})
+  }
 
   user.todos.splice(todo, 1)
-
-  return res.status(204).json()
-
+  
+  return response.status(204).json()
 });
 
 module.exports = app;
--- Ignite/node/chapter-I/03-to-do/template/ignite-template-conceitos-do-nodejs-main/src/server.js
+++ Ignite/node/chapter-I/03-to-do/template/ignite-template-conceitos-do-nodejs-main/src/server.js
@@ -1,3 +1,3 @@
 const app = require('./');
 
-app.listen(1992);
+app.listen(3333);
