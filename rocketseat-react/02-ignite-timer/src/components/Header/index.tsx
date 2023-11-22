238145de35233a5a4786f6eaba3fe0b34a2b601e
Maicon Souza <maicon_souza09@hotmail.com>
2023-11-22 05:17:59 -0300

    22-11-23 05:17

---
+++ rocketseat-react/02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import { HeaderContainer } from './styles'
+import logo from '../../assets/Logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink to="/" title="home">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
